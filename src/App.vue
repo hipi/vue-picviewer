a7c19e2bb6490c80f2c721eaa289dec41e3541ef
chenyeah <yuyehack@gmail.com>
2018-03-21 14:42:52 +0800

    zoom  rote

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <Picview/>
-  </div>
+    <div id="app">
+        <div class="a">
+            <Picview :imgdata="imgdata1" width="100px;" />
+        </div>
+        <div class="a">
+            <Picview :imgdata="imgdata2" :props="{thumbnail:'src',original:'src'}" width="200px;" />
+        </div>
+
+    </div>
 </template>
 
 <script>
-import Picview from './Picview.vue'
+import Picview from "./Picview.vue";
 
 export default {
-  name: 'app',
-  components: {
-    Picview
-  }
-}
+    name: "app",
+    components: {
+        Picview
+    },
+    data() {
+        return {
+            imgdata1: [
+                "https://www.gravatar.com/avatar/40206d98ff6e85da457c341e4a4ea437",
+                "https://www.gravatar.com/avatar/40206d98ff6e85da457c341e4a4ea437"
+            ],
+            imgdata2: [
+                {
+                    src:
+                        "https://www.gravatar.com/avatar/40206d98ff6e85da457c341e4a4ea437"
+                },
+                {
+                    src:
+                        "https://www.gravatar.com/avatar/40206d98ff6e85da457c341e4a4ea437"
+                }
+            ]
+        };
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
-body{
-  margin: 0;
-  padding: 0;
+body {
+    margin: 0;
+    padding: 0;
+}
+.a {
+    margin-bottom: 40px;
 }
 </style>
