db904276e770453a652e475a71340067722d6606
chenyeah <yuyehack@gmail.com>
2018-03-21 11:00:24 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Picview/>
+  </div>
+</template>
+
+<script>
+import Picview from './Picview.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Picview
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body{
+  margin: 0;
+  padding: 0;
+}
+</style>
